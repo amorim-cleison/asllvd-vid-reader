8bbb6382640730900c9b3d8495b523a84acd7a33
Cleison Amorim <cdeamorim@microsoft.com>
2021-01-31 12:16:55 -0300

    Fixing bug on output path composition

--- main.cpp
+++ main.cpp
@@ -13,15 +13,14 @@
 int main(int argc, char *argv[])
 {
 	VidHeader *vid = new VidHeader(argv[1], NULL);
-	char *output_path = argv[2];
-	strcat(output_path, "/%s_%05d.ppm");
-
+	char *output_dir = argv[2];
 	char *prefix = argv[3];
 	int frame_start = atoi(argv[4]);
 	int frame_end = atoi(argv[5]);
 	int fps_in = atoi(argv[6]);
 	int fps_out = atoi(argv[7]);
 
+	const char *output_path = "%s/%s_%05d.ppm";
 	char outfile[1000];
 	int frame_number = (frame_start - 1);
 	int step = fps_in / fps_out;
@@ -34,7 +33,7 @@ int main(int argc, char *argv[])
 	while (frame_number < frame_end)
 	{
 		vid->readFrameFromVideoFile(frame_number);
-		sprintf(outfile, output_path, prefix, (frame_number + 1));
+		sprintf(outfile, output_path, output_dir, prefix, (frame_number + 1));
 		vid->writePPMimage(outfile, vid->pixmap);
 		frame_number += step;
 	}
